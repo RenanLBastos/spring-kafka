37a0c5a0046f62617221a466b0e6352bb9315837
renan <renanlbastos@hotmail.com>
2022-09-25 18:30:04 -0300

    ajustes

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -29,67 +29,8 @@ import java.util.Map;
 
 
 @SpringBootApplication
-@EnableKafka
-@Configuration
 public class KafkaApplication {
-
-
-
     public static void main(String[] args) throws IOException {SpringApplication.run(KafkaApplication.class, args);}
-
-    @Value("${spring.kafka.producer.bootstrap-servers}")
-    String bootstrapAddress;
-
-
-    //configuração produtor
-    @Bean
-    public ProducerFactory<String, Object> multiTypeProducerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> multiTypeKafkaTemplate() {
-        return new KafkaTemplate<>(multiTypeProducerFactory());
-    }
-
-
-
-    //configuração consumidor
-    @Bean
-    public RecordMessageConverter multiTypeConverter() {
-        StringJsonMessageConverter converter = new StringJsonMessageConverter();
-        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
-        typeMapper.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID);
-        typeMapper.addTrustedPackages("*");
-        Map<String, Class<?>> mappings = new HashMap<>();
-        mappings.put("user", UserDto.class);
-        mappings.put("plant", PlantDto.class);
-        converter.setTypeMapper(typeMapper);
-        return converter;
-    }
-
-    @Bean
-    public ConsumerFactory<String, Object> multiTypeConsumerFactory() {
-        HashMap<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> multiTypeKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(multiTypeConsumerFactory());
-        factory.setMessageConverter(multiTypeConverter());
-        return factory;
-    }
-
-
 }
 
 
